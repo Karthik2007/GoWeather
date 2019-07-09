09e316de796afc5255f85c2b0fd84ba7cb983e36
Karthik <Karthik123>
2019-07-09 15:34:09 +0530

    UI failure case flow

--- app/src/main/java/com/karthik/goweather/weather/ui/view/WeatherActivity.kt
+++ app/src/main/java/com/karthik/goweather/weather/ui/view/WeatherActivity.kt
@@ -1,12 +1,15 @@
 package com.karthik.goweather.weather.ui.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.karthik.goweather.R
 import com.karthik.goweather.weather.viewmodel.WeatherViewModel
 import com.karthik.goweather.weather.viewmodel.WeatherViewModelFactory
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_weather.*
+import kotlinx.android.synthetic.main.error_layout.*
 import javax.inject.Inject
 
 class WeatherActivity : DaggerAppCompatActivity() {
@@ -31,15 +34,20 @@ class WeatherActivity : DaggerAppCompatActivity() {
 
     private fun setUpActionListeners() {
 
+        retry_btn.setOnClickListener {
+            weatherViewModel.retryClicked()
+        }
+
     }
 
 
     private fun setUpObservers() {
         weatherViewModel.noInternetOrError.observe(this, Observer {
-
+            error_view.visibility = if (it) View.VISIBLE else View.GONE
         })
 
         weatherViewModel.progressLoading.observe(this, Observer {
+            progress_bar.visibility = if (it) View.VISIBLE else View.GONE
 
         })
 
